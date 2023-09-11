c97b035716ad8525b4087fd0a7d01dd128744e5d
Minami Tomonobu <minami@edanoue.com>
2023-09-12 08:54:21 +0900

    Upgrade to `19.5.716`

--- Scripts/HEU_HoudiniVersion.cs
+++ Scripts/HEU_HoudiniVersion.cs
@@ -41,10 +41,10 @@ namespace HoudiniEngineUnity
         {
                 public const int HOUDINI_MAJOR			= 19;
                 public const int HOUDINI_MINOR			= 5;
-                public const int HOUDINI_BUILD			= 682;
+                public const int HOUDINI_BUILD			= 716;
                 public const int HOUDINI_PATCH			= 0;
 
-                public const string HOUDINI_VERSION_STRING = "19.5.682";
+                public const string HOUDINI_VERSION_STRING = "19.5.716";
 
                 public const int HOUDINI_ENGINE_MAJOR	= 5;
                 public const int HOUDINI_ENGINE_MINOR	= 0;
